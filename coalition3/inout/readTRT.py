0c1157d8281c99262ca69fa8bd3a045555495260
Joel Miro Zeder <Joel.Miro@meteoswiss.ch>
2019-01-16 11:41:24 +0000

    - Added inout/readTRT.py file, which contains the readers   for the raw TRT text files (.trt). This script is analogous to   the readccs4.py file, which contains the readers for CCS4 fields. - Added operational/inputdata.py, which is the calling function for   readin the input data (CCS4 fields and TRT data). - Added the dispalcement functions to the module lagrangian.py (so   far, just the functions to retrieve the flow field were contained)

---
+++ coalition3/inout/readTRT.py
@@ -0,0 +1,55 @@
+""" [COALITION3] Reading functions for CCS4 data"""
+
+from __future__ import division
+from __future__ import print_function
+
+import pandas as pd
+import numpy as np
+
+## =============================================================================
+## FUNCTIONS:
+    
+## Produce array with TRT cell centre pixels 
+def get_vararr_TRT_t0(t0, cfg_set):
+    """Provide vararr style array filled with centre locations of TRT cells"""
+    
+    ## Read filepath of respective TRT file:
+    filepaths, timestamps = path_creator(t0, "TRT", "TRT", cfg_set)
+    cell_info_df = df_empty(cfg_set["TRT_cols"],cfg_set["TRT_dtype"])   
+    filename = "%stmp/%s%s" % (cfg_set["root_path"],
+                               cfg_set["t0"].strftime("%Y%m%d%H%M"),
+                               "_TRT_df.pkl")
+    
+    ## Exception if no TRT-file is available:
+    if filepaths[0] is None:
+        print("   *** Warning: No TRT file found for %s ***" % t0)
+        cell_info_df.to_pickle(filename)
+        vararr = np.zeros((cfg_set["n_integ"],)+cfg_set["xy_ext"])
+        return vararr
+        
+    ## Read in TRT files, get location (CHi,CHj) and TRT variables:
+    traj_IDs, TRTcells, cell_mask = swisstrt.readRdt(filepaths[0])
+    vararr = np.zeros((cfg_set["n_integ"],)+cfg_set["xy_ext"],dtype=np.int32)
+    traj_ID_list = []
+    for traj_ID in traj_IDs:
+        dict_cellinfo = {key:value for key, value in TRTcells[traj_ID].__dict__.items() if not key.startswith('__') and not callable(key)}
+        cell_info_df=cell_info_df.append(pd.DataFrame.from_records([dict_cellinfo],
+                                         index=[9]), ignore_index=True, sort=True)
+        vararr[:,int(TRTcells[traj_ID].iCH),
+                 int(TRTcells[traj_ID].jCH)] = np.int32(traj_ID[8:])
+        traj_ID_list.append(traj_ID)
+    
+    ## Change index to TRT_ID, set dtype for columns and save to disk:
+    cell_info_df.index = traj_ID_list
+    cell_info_df=cell_info_df.astype(cfg_set["type_dict_TRT"],errors='raise')
+    cell_info_df.to_pickle(filename)
+    return vararr
+
+
+## Create empty dataframe with specific columns and datatypes:
+def df_empty(columns, dtypes, index=None):
+    assert len(columns)==len(dtypes)
+    df = pd.DataFrame(index=index)
+    for c,d in zip(columns, dtypes):
+        df[c] = pd.Series(dtype=d)
+    return df
---
+++ coalition3/operational/inputdata.py
@@ -0,0 +1,98 @@
+""" [COALITION3] Reading the input data (from CCS4 domain (Radar, SEVIRI, COSMO, THX)
+    and from TRT)."""
+
+from __future__ import division
+from __future__ import print_function
+
+import datetime
+import numpy as np
+
+from code.inout.readccs4 import get_vararr_t
+from code.inout.readTRT import get_vararr_TRT_t0
+from code.inout.paths import path_creator_vararr
+from code.inout.iotmp import save_file
+
+## =============================================================================
+## FUNCTIONS:
+
+
+## Create numpy arrays of variables for quick access:
+def create_new_vararray(cfg_set,cfg_var):
+    """Create numpy arrays of variables for quick access.
+
+    Parameters
+    ----------
+    
+    cfg_set : dict
+        Basic variables defined in input_NOSTRADAMUS_ANN.py
+    """
+    
+    print("Create new arrays of variables to be displaced...")
+    cfg_var_pred = cfg_var.loc[cfg_var["PREDICTOR"]]
+    
+    ## Differentiate between data sources:
+    """"
+    var_parallel = cfg_var_pred["VARIABLE"].loc[cfg_var_pred["SOURCE"]=="RADAR"].tolist()
+    if cfg_set["displ_TRT_cellcent"]: var_parallel = var_parallel.append("TRT")
+    num_cores = np.max([multiprocessing.cpu_count()-2,1])
+    print("  Parallelising reading of RADAR/TRT variable with %s cores" % num_cores)
+    Parallel(n_jobs=num_cores)(delayed(create_new_vararray_core)(cfg_set,var) for var in var_parallel)
+    """
+    
+    ## Loop over variables for displacement:
+    create_new_vararray_core(cfg_set,"TRT")
+    source = None
+    
+    for var in cfg_set["var_list"]:
+        source_new = cfg_var["SOURCE"].loc[cfg_var["VARIABLE"]==var].values[0]
+        if source_new!=source or var==cfg_set["var_list"][-1]:
+            t2 = datetime.datetime.now()
+            if source is not None: # and cfg_set["verbose"]:
+                print("   Elapsed time for the reading %s variables: %s " % (source,t2-t1))
+            t1 = datetime.datetime.now(); source = source_new
+        create_new_vararray_core(cfg_set,var)
+    
+    ## In case the verification should be performed, initialise array with additional information:
+    if cfg_set["verify_disp"]:
+        filename_verif_stat = "%stmp/%s_%s_stat_verif.npy" % (cfg_set["root_path"],
+                                cfg_set["verif_param"],str(cfg_set[cfg_set["verif_param"]]))
+        stat_array = np.zeros((1,len(cfg_set["var_list"]),int(cfg_set["n_stat"])),
+                               dtype=np.float16)-9999.
+        np.save(filename_verif_stat, stat_array)
+
+
+## Create variable array of specific variable:
+def create_new_vararray_core(cfg_set,var):
+    """Create variable array of specific variable."""
+    if var in ["U_OFLOW","V_OFLOW"]:
+        return
+    
+    t1 = datetime.datetime.now()
+    if cfg_set["verbose"]: print("  ... new "+var+" array created in:")
+    filename = path_creator_vararr("orig",var,cfg_set)
+    vararr = np.zeros((cfg_set["n_integ"],)+cfg_set["xy_ext"])
+    
+    ## Get field of every time step (if map-function cannot be applied)
+    i = 0
+    t_delta = np.array(range(cfg_set["n_integ"]))*datetime.timedelta(minutes=cfg_set["timestep"])
+    if var == "TRT":
+        vararr = get_vararr_TRT_t0(cfg_set["t0"], cfg_set)
+    else:
+        for t_d in t_delta:
+            t_current = cfg_set["t0"] - cfg_set["time_change_factor"]*t_d
+            vararr_t = get_vararr_t(t_current, var, cfg_set)
+            vararr[i,:,:] = vararr_t[0,:,:]
+            i += 1
+    save_file(filename, data_arr=vararr,var_name=var,cfg_set=cfg_set)
+    if cfg_set["verbose"]: print("      "+filename)
+        
+    ## In case verification of displacements should be performed, also initialise skill-score array:
+    if cfg_set["verify_disp"]:
+        filename_verif = "%stmp/%s_%s_%s_verif.npy" % (cfg_set["root_path"],
+                         cfg_set["verif_param"],str(cfg_set[cfg_set["verif_param"]]), var)
+        verif_array = np.zeros((1,len(cfg_set["scores_list"]),cfg_set["n_integ"]-1))-9999.
+        np.save(filename_verif, verif_array)
+    
+    t2 = datetime.datetime.now()
+    if False: print("    Elapsed time for creation of variable %s: %s" % (var,str(t2-t1)))
+    
