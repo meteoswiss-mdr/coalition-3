fce9e9b3537c16d92019bc613cce04027c78132f
Joel Miro Zeder <Joel.Miro@meteoswiss.ch>
2019-01-16 08:25:14 +0000

    Changed bash_script 'add_PYTHONPATH' to insert new 'coalition3/' directory

--- add_PYTHONPATH.sh
+++ add_PYTHONPATH.sh
@@ -1,4 +1,4 @@
 dot="$(cd "$(dirname "$0")"; pwd)"
-path="$dot/code"
+path="$dot/coalition3"
 echo   Addin path $path to PYTHONPATH env variable in .bash_profile
 echo export 'PYTHONPATH=$PYTHONPATH:'$path  >> ~/.bash_profile
