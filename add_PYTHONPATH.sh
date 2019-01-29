1ad985ee5c8b359a3e5dfcfa2d176385896099f1
Joel Miro Zeder <Joel.Miro@meteoswiss.ch>
2019-01-29 14:49:34 +0000

    Changed "add_PYTHONPATH.sh" shell script
    - This shell scripts now adds the "coalition-3" repo, and not the
      "coalition3" subdirectory containing the scripts anymore to the
      ".bash_profile" script, where it is then added to the PYTHONPATH.
    - Furthermore, the script also sources the ".bash_profile" script
      at the end, so it doesn't have to be done manually anymore.

--- add_PYTHONPATH.sh
+++ add_PYTHONPATH.sh
@@ -1,4 +1,6 @@
 dot="$(cd "$(dirname "$0")"; pwd)"
-path="$dot/coalition3"
+#path="$dot/coalition3"
+path="$dot/"
 echo   Addin path $path to PYTHONPATH env variable in .bash_profile
 echo export 'PYTHONPATH=$PYTHONPATH:'$path  >> ~/.bash_profile
+source ~/.bash_profile
