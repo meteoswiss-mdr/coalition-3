9aac2b4326a8aab8651dd64b3c4196b4cbc4994e
Joel Miro Zeder <Joel.Miro@meteoswiss.ch>
2019-01-29 09:35:51 +0000

    Changed import matplotlib.pyplot as plt to import matplotlib.pylab as plt

--- coalition3/verification/skillscores.py
+++ coalition3/verification/skillscores.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 
 import datetime
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pylab as plt
 import pysteps as st
 #from pysteps.vf import scores_det_cat_fcst
 
