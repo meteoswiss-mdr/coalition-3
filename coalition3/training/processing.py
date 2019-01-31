22edb17c95dd30c9b756e86cf1696a6b2c7fcb90
Joel Miro Zeder <Joel.Miro@meteoswiss.ch>
2019-01-31 10:15:02 +0000

    Changed path creation

--- coalition3/training/processing.py
+++ coalition3/training/processing.py
@@ -113,8 +113,8 @@ def change_form_width_statistics(factor,cfg_set_input,cfg_var,cfg_var_combi):
     print("Change form width from %02dkm to %02dkm and repeat reading in statistics:" %
           (cfg_set_input["stat_sel_form_width"],new_form_width))
     cfg_set_input["stat_sel_form_width"] = new_form_width
-    cfg_set_input["stat_sel_form_size"] = Nip.form_size(cfg_set_input["stat_sel_form_width"],
-                                                        cfg_set_input["stat_sel_form"])
+    cfg_set_input["stat_sel_form_size"]  = sta.form_size(cfg_set_input["stat_sel_form_width"],
+                                                         cfg_set_input["stat_sel_form"])
     
     ## Get new indices from which to read the statistics:
     t1 = datetime.datetime.now()
@@ -132,7 +132,7 @@ def change_form_width_statistics(factor,cfg_set_input,cfg_var,cfg_var_combi):
 
 ## Clean up displacement arrays in /tmp directory:
 def clean_disparr_vararr_tmp(cfg_set,fix_t0_str=None):
-    path = "%stmp/" % (cfg_set["root_path"])
+    path = cfg_set["tmp_output_path"]
     #print("Remove all displacement files from temporary directory %s" % path)
     check_for_npz = cfg_set["save_type"]=="npy"
     
