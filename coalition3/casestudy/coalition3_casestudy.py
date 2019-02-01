0e3b58825ee11e431634d1f753f8ee80d1131e21
Joel Miro Zeder <Joel.Miro@meteoswiss.ch>
2019-02-01 08:31:40 +0000

    Added script to perform casestudy of 07.07.2015
    - Script should generate output needed for EUMETSAT Meteorological
      Satellite Conference poster, but it is not tested and does
      probably not work straight away.

--- coalition3/casestudy/coalition3_casestudy.py
+++ coalition3/casestudy/coalition3_casestudy.py
@@ -1,11 +1,4 @@
-#!/opt/users/common/packages/anaconda2//bin
-
-""" Input generation for NOSTRADAMUS artificial neural network:
-
-Input fields for NOSTRADAMUS are spatially displaced by the motion 
-fields derived from an optical flow algorithm, using radar rain
-rates (RZC) to derive the motion vectors.
-"""
+""" [COALITION3] Create output for EUMETSAT Conference poster """
 
 # ===============================================================================
 # Import packages and functions
@@ -13,33 +6,27 @@ rates (RZC) to derive the motion vectors.
 from __future__ import division
 from __future__ import print_function
 
-import sys
-import configparser
-import datetime
 import os
-import pdb #pdb.set_trace()
+import pdb
+import sys
+import datetime
+import configparser
 import numpy as np
 
-import pysteps as st
-import NOSTRADAMUS_1_input_prep_fun as Nip
-import NOSTRADAMUS_1_casestudy_fun as Ncs
+import coalition3.inout.readconfig as cfg
+import coalition3_casestudy_fun as ccs
+
 sys.path.insert(0, '/data/COALITION2/database/radar/ccs4/python')
 import metranet
 
 ## ===============================================================================
 ## Make static setting
 
-## Define path to config file
-CONFIG_PATH = "/data/COALITION2/PicturesSatellite/results_JMZ/2_input_NOSTRADAMUS_ANN"
-CONFIG_FILE_set = "NOSTRADAMUS_1_input_prep_longrun.cfg"
-CONFIG_FILE_var = "cfg_var.csv"
-
 ## Input basics
 #t_start   = datetime.datetime(2015, 7, 7, 16, 15)
 #t_end     = datetime.datetime(2015, 7, 7, 18, 30)
 t_start   = datetime.datetime(2015, 7, 7, 11, 45)
 t_end     = datetime.datetime(2015, 7, 7, 15, 45)
-
 t_end_alt = datetime.datetime(2015, 7, 7, 18, 30)
 
 #x_range   = (300,355)#(300,370)
@@ -50,8 +37,8 @@ x_range   = (300,500)#(300,370)
 y_range   = (150,350)#(160,230)
 
 t_end_str = datetime.datetime.strftime(t_end, "%Y%m%d%H%M")
-cfg_set, cfg_var = Nip.get_config_info(CONFIG_PATH,CONFIG_FILE_set,CONFIG_FILE_var,t_end_str)
-cfg_set = Ncs.update_cfg_set(cfg_set,t_start,t_end,x_range,y_range,t_end_alt)
+cfg_set, cfg_var, cfg_var_combi = cfg.get_config_info_op()
+cfg_set = ccs.update_cfg_set(cfg_set,t_start,t_end,x_range,y_range,t_end_alt)
       
 print('\n\nStart NOSTRADAMUS case study with:'+
 '\n  Starttime:      '+str(datetime.datetime.strftime(t_start, "%d.%m.%Y %H:%M"))+
@@ -62,13 +49,13 @@ print('\n\nStart NOSTRADAMUS case study with:'+
 
 ## ===============================================================================
 ## Get data (orig and displaced) of the respective time frame
-Ncs.subset_disp_data(cfg_set, resid=True)
+ccs.subset_disp_data(cfg_set, resid=True)
 
 ## Plot temporal statistics of data subset (variable-specific)
-#Ncs.subset_stats(cfg_set)
+#ccs.subset_stats(cfg_set)
 
 ## Plot some displacement fields
-#Ncs.disp_field_plot(cfg_set)
+#ccs.disp_field_plot(cfg_set)
 
 
 
