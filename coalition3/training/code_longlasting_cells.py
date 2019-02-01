08e5242b7acd5157114d48caec188362322be19f
Joel Miro Zeder <Joel.Miro@meteoswiss.ch>
2019-02-01 09:11:58 +0000

    Added code collection to retrive long lasting TRT cells
    - This code collection performs a counting of the duration of
      the TRT cells within the dataset.

--- coalition3/training/code_longlasting_cells.py
+++ coalition3/training/code_longlasting_cells.py
@@ -1,36 +1,21 @@
-# coding: utf-8
+""" [COALITION3] Code to get long lasting TRT cells """
+
 import xarray as xr
 import numpy as np
-xr_loc = xr.open_dataset("diam_16km/Combined_stat_pixcount.nc")
-xr_loc
-get_ipython().magic(u'll -h diam_16km/')
-TRT_ID = xr_loc.DATE_TRT_ID
-TRT_ID = 
-TRT_ID 
-TRT_ID = [TRT_ID_i[12:] for TRT_ID_i in TRT_ID] 
-TRT_ID = [TRT_ID_i[12:] for TRT_ID_i in TRT_ID.values] 
-TRT_ID
-TRT_ID = [TRT_ID_i[1:] for TRT_ID_i in TRT_ID.values] 
-TRT_ID = [TRT_ID_i[1:] for TRT_ID_i in TRT_ID] 
-TRT_ID
-len(TRT_ID)
-len(TRT_ID.shape)
-TRT_ID.shape
-np.unique(TRT_ID)
-len(np.unique(TRT_ID))
-from collections import Counter
-TRT_ID_count = Counter(TRT_ID)
-TRT_ID_count
-TRT_ID_count_sort = [(key,value) for key, value in sorted(TRT_ID_count.iteritems(), key=lambda (k,v): (v,k))]
-TRT_ID_count_sort
 import pandas as pd
-pd.DataFrame.from_dict(TRT_ID_count)
-np.array(TRT_ID_count_sort)
-np.array(TRT_ID_count_sort).shape
-TRT_ID_count_sort_pd = pd.DataFrame(np.array(TRT_ID_count_sort)))
-TRT_ID_count_sort_pd = pd.DataFrame(np.array(TRT_ID_count_sort))
+from collections import Counter
+
+xr_loc = xr.open_dataset("<Add your path here>")
+xr_loc
+
+TRT_ID = xr_loc.DATE_TRT_ID
+TRT_ID = [TRT_ID_i[13:] for TRT_ID_i in TRT_ID.values] 
+len(TRT_ID)
+
+len(np.unique(TRT_ID))
+TRT_ID_count = Counter(TRT_ID)
+TRT_ID_count_sort = [(key,value) for key, value in sorted(TRT_ID_count.iteritems(), key=lambda (k,v): (v,k))]
+
+TRT_ID_count_sort_pd = pd.DataFrame(np.array(TRT_ID_count_sort),columns=["TRT_ID","Count"])
 TRT_ID_count_sort_pd
-TRT_ID_count_sort_pd.info
 TRT_ID_count_sort_pd.info()
-xrange(10)
-get_ipython().magic(u'save get_longlasting_cells.py ~0/')
