7b418469b2fe9d4be3180c57cbc84f2ff6f1165a
Joel Miro Zeder <Joel.Miro@meteoswiss.ch>
2019-02-08 10:32:53 +0000

    Minor changes

--- coalition3/statlearn/script_EDA_df.py
+++ coalition3/statlearn/script_EDA_df.py
@@ -14,6 +14,32 @@ import matplotlib.pylab as plt
 
 import coalition3.inout.paths as pth
 
+def plot_feature_importance(model,delta_t):
+    sort_ind = np.argsort(model.feature_importances_)[::-1]
+    df_featimp = pd.DataFrame(np.array([X.columns[sort_ind[:500]],
+                                        model.feature_importances_[sort_ind[:500]]]).T,
+                              columns=["Feature","Importance"])
+    df_featimp.plot(drawstyle="steps", linewidth=2)
+    plt.grid(); plt.ylabel("Feature importance"); plt.xlabel("Features (sorted)")
+    plt.title("Feature importance - TRT t+%imin" % delta_t)
+    #plt.xticks(rotation=20)
+    plt.tight_layout()
+    plt.savefig("Feature_importance_%imin_long.pdf" % delta_t,orientation="portrait")
+
+    fig = plt.figure(figsize = [10,10])
+    ax1 = fig.add_subplot(2,1,1)
+    xgb.plot_importance(model,ax1,max_num_features=20,importance_type="weight",
+                        title="Feature importance (Weight) - TRT t+%imin" % delta_t)
+    ax2 = fig.add_subplot(2,1,2)
+    xgb.plot_importance(model,ax2,max_num_features=20,importance_type="gain",
+                        title="Feature importance (Gain) - TRT t+%imin" % delta_t,show_values=False)
+    #ax3 = fig.add_subplot(3,1,3)
+    #plot_importance(model,ax3,max_num_features=20,importance_type="cover")
+    #plt.subplots_adjust(left=0.3,right=0.95)
+    plt.tight_layout()
+    plt.savefig("Feature_importance_%imin.pdf" % delta_t,orientation="portrait")
+
+
 ## ============================================================================
 ## Open pandas training dataframe:
 user_argv_path = sys.argv[1] if len(sys.argv)==2 else None
@@ -21,6 +47,7 @@ path_to_df = pth.file_path_reader("pandas training dataframe",user_argv_path)
 df = pd.read_hdf(path_to_df,key="df")
 
 ## Make analysis of how many values are missing per column:
+"""
 df_nan_count_sort = df.isna().sum().sort_values(ascending=False)
 df_nan_count_sort[:6].plot(drawstyle="steps", linewidth=2)
 plt.grid(); plt.ylabel("NaN count"); plt.xlabel("Features (sorted)")
@@ -28,6 +55,7 @@ plt.title("Number of missing values per feature")
 plt.xticks(rotation=20)
 plt.tight_layout()
 plt.show()
+"""
 
 ## Analyse pearson correlation between features (VERY TIME CONSUMING!):
 """
@@ -63,45 +91,21 @@ plt.grid()
 plt.show()
 """
 
-
-
 ## Delete rows with nan-entries:
 df_nonnan = df.dropna(0,'any')
 df.to_hdf("df_23km_nonnan.h5",key="df_nonnan",mode="w",complevel=0)
 
 ## Choose columns not containing TRT
 X = df_nonnan[[Xcol for Xcol in df_nonnan.columns if "TRT" not in Xcol]]
+y_10 = df_nonnan[["TRT_Rank|10"]]
 y_30 = df_nonnan[["TRT_Rank|30"]]
 
-
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
-#model_30 = XGBRegressor(n_jobs=6,nthreads=6) #objective=reg:linear
+#os.environ['KMP_DUPLICATE_LIB_OK']='True'
+model_10 = xgb.XGBRegressor(n_jobs=6,nthreads=6) #objective=reg:linear
+model_10.fit(X, y_10, verbose=True)
 model_30 = xgb.XGBRegressor(n_jobs=6,nthreads=6) #objective=reg:linear
 model_30.fit(X, y_30, verbose=True)
+with open("model_10_.pkl","wb") as file: pickle.dump(model_10,file,protocol=-1)
 with open("model_30.pkl","wb") as file: pickle.dump(model_30,file,protocol=-1)
-
-def plot_feature_importance(model,delta_t):
-    sort_ind = np.argsort(model.feature_importances_)[::-1]
-    df_featimp = pd.DataFrame(np.array([X.columns[sort_ind[:500]],
-                                        model.feature_importances_[sort_ind[:500]]]).T,
-                              columns=["Feature","Importance"])
-    df_featimp.plot(drawstyle="steps", linewidth=2)
-    plt.grid(); plt.ylabel("Feature importance"); plt.xlabel("Features (sorted)")
-    plt.title("Feature importance - TRT t+%imin" % delta_t)
-    #plt.xticks(rotation=20)
-    plt.tight_layout()
-    plt.savefig("Feature_importance_%imin_long.pdf" % delta_t,orientation="portrait")
-
-    fig = plt.figure(figsize = [10,10])
-    ax1 = fig.add_subplot(2,1,1)
-    xgb.plot_importance(model,ax1,max_num_features=20,importance_type="weight",
-                        title="Feature importance (Weight) - TRT t+%imin" % delta_t)
-    ax2 = fig.add_subplot(2,1,2)
-    xgb.plot_importance(model,ax2,max_num_features=20,importance_type="gain",
-                        title="Feature importance (Gain) - TRT t+%imin" % delta_t,show_values=False)
-    #ax3 = fig.add_subplot(3,1,3)
-    #plot_importance(model,ax3,max_num_features=20,importance_type="cover")
-    #plt.subplots_adjust(left=0.3,right=0.95)
-    plt.tight_layout()
-    plt.savefig("Feature_importance_%imin.pdf" % delta_t,orientation="portrait")
-"""
+plot_feature_importance(model_10,10)
+plot_feature_importance(model_30,30)
