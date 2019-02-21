f01b6f4a8d1ffc80b1afa7c1dfc81676afd825ec
Joel Miro Zeder <Joel.Miro@meteoswiss.ch>
2019-02-21 12:47:53 +0000

    Assured correct TRT Rank calculation
    - Now the VIL, MaxEcho, ET45 and 'number-of-pixels-equallarger-than-57'
      variables are all correctly cut-off at too high (or low) value.

--- coalition3/operational/statistics.py
+++ coalition3/operational/statistics.py
@@ -683,11 +683,11 @@ def calc_TRT_Rank(xr_stat,ET_option="cond_median"):
     
     ## Scale variables to values between min and max according to Powerpoint Slide
     ## M:\lom-prod\mdr-prod\oper\adula\Innovation\6224_COALITION2\06-Presentations\2018-10-17_TRT_Workshop-DACH_MWO_hea.pptx:
-    VIL_scal.values[VIL_scal.values>65.]   = 65. ## Max  VIL:       56 kg m-2
-    ME_scal.values[ME_scal.values<45.]     = 45. ## Min MaxEcho:    45 dBZ
-    ME_scal.values[ME_scal.values>57.]     = 57. ## Max MaxEcho:    57 dBZ
-    ET45_scal.values[ET45_scal.values>10.] = 10. ## Max EchoTop:    10 km
-    A55_scal.values[A55_scal.values>40]    = 40  ## Max pix >57dBZ: 40
+    VIL_scal  = VIL_scal.where(VIL_scal<65.,65.)   ## Max  VIL:       65 kg m-2
+    ME_scal   = ME_scal.where(ME_scal>45.,45.)     ## Min MaxEcho:    45 dBZ
+    ME_scal   = ME_scal.where(ME_scal<57.,57.)     ## Max MaxEcho:    57 dBZ
+    ET45_scal = ET45_scal.where(ET45_scal<10.,10.) ## Max EchoTop:    10 km
+    A55_scal  = A55_scal.where(A55_scal<40,40)     ## Max pix >57dBZ: 40
 
     ## Scale variables to values between 0 and 4:
     VIL_scal  = VIL_scal/65.*4
