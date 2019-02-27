68d5b32693c2b02611fd26ae3b2fc4011d16d75a
Joel Miro Zeder <Joel.Miro@meteoswiss.ch>
2019-02-27 12:54:29 +0000

    Added new features to scatterplot

--- coalition3/statlearn/script_EDA_df.py
+++ coalition3/statlearn/script_EDA_df.py
@@ -15,10 +15,13 @@ import seaborn as sns
 import datetime as dt
 import matplotlib.pylab as plt
 import matplotlib.colors as mcolors
+import matplotlib.patheffects as pe
+import matplotlib.patches as patches
 
 import coalition3.inout.paths as pth
 import coalition3.inout.readconfig as cfg
-from coalition3.visualisation.TRTcells import contour_of_2dHist
+import coalition3.training.preparation as prep
+import coalition3.visualisation.TRTcells as visTRT
 
 def get_X_col(colname):
     ## No future TRT observations in input:
@@ -26,15 +29,34 @@ def get_X_col(colname):
                "-" in colname or "|0" in colname) and \
               ("TRT_Rank_diff" not in colname)
     return(use_col)
+
+def plot_hist_TRT_Ranks(df_plot,cfg_tds):
+    fig_hist, axes = plt.subplots(1, 1)
+    fig_hist.set_size_inches(8, 5)
+
+    ## Analyse distribution of ranks
+    axes = visTRT.plot_band_TRT_col(axes,df_plot["RANKr"],18000,4000,arrow_start=25000)
+    df_plot["RANKr"] = df_plot["RANKr"]/10.
+    df_plot["RANKr"].hist(ax=axes,bins=np.arange(0,4.125,0.125),facecolor=(.7,.7,.7),alpha=0.75,grid=True)
+    axes.set_xlabel("TRT rank")
+    axes.set_title("TRT Rank Distribution")
+    df_plot["RANKr"] = df_plot["RANKr"]*10.
+    plt.savefig(os.path.join(cfg_tds["fig_output_path"],"TRT_Rank_hist_post.pdf"), orientation="landscape")
     
-def plot_TRT_scatter(df_plot,list_min_plus,path_addon="",contour=False):
+def plot_TRT_scatter(df_plot,list_min_plus,path_addon="",contour=False,TRTcol=False,model_borders_ls=None):
     df_plot_i = df_plot
     if isinstance(df_plot, list) and len(df_plot)!=len(list_min_plus):
         raise ValueError("If list of df is provided, the list of time deltas must be of the same length")
     fig, axes = plt.subplots(nrows=1, ncols=len(list_min_plus), figsize=[4*len(list_min_plus)+1,8])
+    props = dict(boxstyle='round', facecolor='white') #, alpha=0.5
+
     for ax_i, min_plus in enumerate(list_min_plus):
         if isinstance(df_plot, list):
             df_plot_i = df_plot[ax_i]
+            
+        if TRTcol:
+            axes[ax_i] = visTRT.plot_band_TRT_col(axes[ax_i],df_plot_i["TRT_Rank|0"]*10,2.6,0.4,arrow_start=2.2)
+            
         counts,ybins,xbins,image = axes[ax_i].hist2d(df_plot_i["TRT_Rank|0"],df_plot_i["TRT_Rank_diff|%i" % min_plus],
                                                   bins=[200,400],range=[[0,4],[-4,4]],norm=mcolors.LogNorm(),cmap="magma")
         #cbar = fig.colorbar(image, ax=axes[ax_i], extend='max')
@@ -42,24 +64,47 @@ def plot_TRT_scatter(df_plot,list_min_plus,path_addon="",contour=False):
         axes[ax_i].grid()
         #axes[ax_i].plot([0,4],[0,-4],'w--',linewidth=2) #,facecolor="w",linewidth=2,linestyle='--')
         if contour:
-            cont2d_1, lvl = contour_of_2dHist(counts,smooth=True)
+            cont2d_1, lvl = visTRT.contour_of_2dHist(counts,smooth=True)
             CS = axes[ax_i].contour(cont2d_1,levels=lvl,linewidths=2,cmap="YlGn_r",extent=[0,4,-4,4]) #,extent=[xbins.min(),xbins.max(),ybins.min(),ybins.max()])
             CS_lab = axes[ax_i].clabel(CS, inline=1, fontsize=10, fmt='%i%%', colors="black")
             #[txt.set_backgroundcolor('white') for txt in CS_lab]
             [txt.set_bbox(dict(facecolor='white', edgecolor='none', pad=0.3, boxstyle='round', alpha=0.7)) for txt in CS_lab] #pad=0,
-        axes[ax_i].set_ylabel(r"TRT Rank change  $t_{+%imin}$" % min_plus) #; axes.set_title('TRT Ranks (16km diameter)')
-        axes[ax_i].set_xlabel(r"TRT Rank  $t_0$")
+        axes[ax_i].set_ylabel(r"TRT Rank change  t$\mathregular{_{+%imin}}$" % min_plus) #; axes.set_title('TRT Ranks (16km diameter)')
+        axes[ax_i].set_xlabel(r"TRT Rank  $\mathregular{t_0}$")
         axes[ax_i].set_title(r"Time Delta +%imin" % min_plus)
         axes[ax_i].set_aspect('equal')
         axes[ax_i].patch.set_facecolor('0.7')
         axes[ax_i].fill_between([0,4], y1=[-4,-4], y2=[0,-4], facecolor="none", hatch="X", edgecolor="darkred", linewidth=0.5)
         axes[ax_i].fill_between([0,4], y1=[ 4, 0], y2=[4, 4], facecolor="none", hatch="X", edgecolor="darkred", linewidth=0.5)
         str_n_cells = "Total number of\ncells:   %i" % np.sum(counts)
-        props = dict(boxstyle='round', facecolor='white') #, alpha=0.5
         axes[ax_i].text(0.05, 0.08, str_n_cells, transform=axes[ax_i].transAxes, #fontsize=8,
                         verticalalignment='top', bbox=props)
         #axes[ax_i].text(0.4,3.6,str_n_cells)
-
+            
+        if model_borders_ls is not None:
+            model_names = ["low","med","high"]
+            model_borders = [0]+model_borders_ls+[4]
+            for bord_i, border in enumerate(model_borders):
+                #if border not in [0,4]:
+                #    axes[ax_i].axvline(border,color="white",linestyle="--",zorder=1)
+                if border!=0:
+                    n_samples = np.sum(np.logical_and(df_plot_i["TRT_Rank|0"].values<border,
+                                                      df_plot_i["TRT_Rank|0"].values>=model_borders[bord_i-1]))
+                    axes[ax_i].arrow(model_borders[bord_i-1],3.5,border-model_borders[bord_i-1],0,
+                                     color="white", length_includes_head=True, head_width=0.07)
+                    axes[ax_i].arrow(border,3.5,model_borders[bord_i-1]-border,0,
+                                     color="white", length_includes_head=True, head_width=0.07)
+                    text_loc = border-(border-model_borders[bord_i-1])/2.
+                    if len(model_borders)==4:
+                        text_str = r"Mod$\mathregular{_{%s}}$" % model_names[bord_i-1]
+                        text_str += "\n%i" % n_samples
+                    else:
+                        text_str = r"Mod$_{%1.1f}$" % border
+                    axes[ax_i].text(text_loc/4., 7.5/8, text_str,
+                        transform=axes[ax_i].transAxes, fontsize=8,
+                        verticalalignment='center', horizontalalignment='center',
+                        bbox=props)
+                        
     plt.tight_layout()
     path_addon_num = "_".join([str(num) for num in list_min_plus])
     if len(path_addon)>0: path_addon = "_"+path_addon
@@ -74,7 +119,17 @@ col30 = '#D55E00'
 ## Open pandas training dataframe:
 user_argv_path = sys.argv[1] if len(sys.argv)==2 else None
 path_to_df = pth.file_path_reader("pandas training dataframe",user_argv_path)
-df = pd.read_hdf(path_to_df,key="df")
+
+import_nonnan = False
+if os.path.exists("%s_nonnan.h5" % os.path.splitext(path_to_df)[0]):
+    import_ans = ""
+    while (import_ans!="y" and import_ans!="n"):
+        import_ans = raw_input("  Dataframe ending '.. _nonnan.h5' already exists, import this one? [y/n] ")
+    if import_ans=="y":
+        df_nonnan = pd.read_hdf("%s_nonnan.h5" % os.path.splitext(path_to_df)[0],"df_nonnan")
+        import_nonnan = True
+if not import_nonnan:
+    df = pd.read_hdf(path_to_df,key="df")
 
 """
 ## Make analysis of how many values are missing per column:
@@ -122,11 +177,19 @@ del(df_pears_corr_feat)
 """
 
 ## Delete rows with nan-entries:
-print("Dropping NaN values")
-df_nonnan = df.dropna(0,'any')
-df_nonnan.to_hdf("%s_nonnan.h5" % os.path.splitext(path_to_df),
-                 key="df_nonnan",mode="w",complevel=0)
-#df_nonnan = pd.read_hdf("df_23km_nonnan.h5","df_nonnan")
+if not import_nonnan:
+    print("Dropping NaN values")
+    df_nonnan = df.dropna(0,'any')
+    df_nonnan.to_hdf("%s_nonnan.h5" % os.path.splitext(path_to_df)[0],
+                     key="df_nonnan",mode="w",complevel=0)
+
+## Get map of cell locations and distributions
+df_nonnan = prep.change_append_TRT_cell_info(cfg_tds,df=df_nonnan)
+df_nonnan["date"] = [DTI[:12] for DTI in df_nonnan.index.values]
+plot_hist_TRT_Ranks(df_nonnan,cfg_tds)
+#df_nonnan["date"] = df_nonnan["date"].astype(np.datetime64,copy=False)
+prep.exploit_TRT_cell_info(cfg_tds,samples_df=df_nonnan)
+df_nonnan["RANKr"] = df_nonnan["RANKr"]*10
 
 ## Construct selection criteria for input dataset:
 print("Split in 10min and 30min forcast")
@@ -170,7 +233,7 @@ df_nonnan_nonzerot0 = df_nonnan.loc[df_nonnan["TRT_Rank|0"]>=0.15]
 plot_TRT_scatter(df_nonnan_nonzerot0,[10,30],"nonzerot0")
 df_nonnan_nonzerot0t10 = df_nonnan.loc[(df_nonnan["TRT_Rank|10"]>=0.15) & (df_nonnan["TRT_Rank|0"]>=0.15)]
 df_nonnan_nonzerot0t30 = df_nonnan.loc[(df_nonnan["TRT_Rank|30"]>=0.15) & (df_nonnan["TRT_Rank|0"]>=0.15)]
-plot_TRT_scatter([df_nonnan_nonzerot0t10,df_nonnan_nonzerot0t30],[10,30],path_addon="nonzerot0t10t30",contour=True)
+plot_TRT_scatter([df_nonnan_nonzerot0t10,df_nonnan_nonzerot0t30],[10,30],path_addon="nonzerot0t10t30",contour=True,TRTcol=True,model_borders_ls=[1.2,2.3])
 
 
 
