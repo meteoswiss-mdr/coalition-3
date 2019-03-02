583e518297053409de614274cb852c98287cdc30
Joel Zeder <joelmiro.zeder@meteoswiss.ch>
2019-03-02 09:50:08 +0100

    The module feature now uses function 'get_model_input()'
    - The conversion of the training dataset to model input is now based on the
      function 'get_model_input()' from the module 'inputprep.py'.

--- coalition3/statlearn/feature.py
+++ coalition3/statlearn/feature.py
@@ -20,11 +20,12 @@ import matplotlib.colors as mcolors
 
 from sklearn.model_selection import train_test_split
 
+import coalition3.statlearn.inputprep as ipt
 from coalition3.visualisation.TRTcells import contour_of_2dHist
 
 ## =============================================================================
 ## FUNCTIONS:
-  
+
 ## Get Mean Square Error (MSE) for different number of n features:
 def fit_model_n_feat(X_train, y_train, top_features, n_feat, n_feat_arr):
     perc_finished = np.sum(n_feat_arr[:np.where(n_feat_arr==n_feat)[0][0]],
@@ -35,11 +36,11 @@ def fit_model_n_feat(X_train, y_train, top_features, n_feat, n_feat_arr):
     model.fit(X_train[top_features.index[:n_feat]], y_train)
     return(model)
     #return(mean_squared_error(y_test, model.predict(X_test[top_features.index[:n_feat]])))
-    
+
 ## Get Mean Square Error (MSE) for different number of n features:
 def mse_n_feat(X_test, y_test, top_features, n_feat, model):
     return(met.mean_squared_error(y_test, model.predict(X_test[top_features.index[:n_feat]])))
-    
+
 ## Plotting procedure for feature importance:
 def plot_feature_importance(model,X,delta_t,cfg_tds,mod_name):
     sort_ind = np.argsort(model.feature_importances_)[::-1]
@@ -66,14 +67,6 @@ def plot_feature_importance(model,X,delta_t,cfg_tds,mod_name):
     plt.tight_layout()
     plt.savefig(os.path.join(cfg_tds["fig_output_path"],"Feature_importance_%imin%s.pdf" % (delta_t,mod_name)),orientation="portrait")
 
-## Get columns not containing TRT of the future:
-def get_X_col(colname):
-    ## No future TRT observations in input:
-    use_col = ("TRT" not in colname or \
-               "-" in colname or "|0" in colname) and \
-              ("TRT_Rank_diff" not in colname)
-    return(use_col)
-
 ## Get feature ranking for the complete dataset:
 def get_feature_importance(df_nonnan_nonzerot0,pred_dt,cfg_tds,model_path,mod_bound=None,mod_name=""):
     print("Get features for lead time t0 + %imin" % pred_dt, end="")
@@ -88,7 +81,7 @@ def get_feature_importance(df_nonnan_nonzerot0,pred_dt,cfg_tds,model_path,mod_bo
     else:
         print(" (for all samples)")
     sys.stdout.flush()
-    
+
     ## Check whether model already exists:
     if os.path.exists(os.path.join(model_path,"model_%i%s_t0diff_maxdepth6.pkl" % (pred_dt,mod_name))):
         use_existing = ""
@@ -97,18 +90,22 @@ def get_feature_importance(df_nonnan_nonzerot0,pred_dt,cfg_tds,model_path,mod_bo
         if use_existing=="n":
             print("  Use existing one, return from this function")
             return
-    
+
     ## Delete rows with TRT Rank close to zero at lead time:
     print("  Delete rows with TRT Rank close to zero at lead time")
+    X, y = ipt.get_model_input(df_nonnan_nonzerot0, del_TRTeqZero_tpred=True,
+            split_Xy=True, pred_dt=pred_dt, TRTRankt0_bound=mod_bound)
+    """
     if mod_bound is not None:
         df_nonnan_nonzerot0 = df_nonnan_nonzerot0.loc[(df_nonnan_nonzerot0["TRT_Rank|0"]>=mod_bound[0]) & \
                                                       (df_nonnan_nonzerot0["TRT_Rank|0"]<mod_bound[1])]
     X = df_nonnan_nonzerot0.loc[df_nonnan_nonzerot0["TRT_Rank|%i" % pred_dt]>=0.15,
-                                [Xcol for Xcol in df_nonnan_nonzerot0.columns if get_X_col(Xcol)]]
+                                [Xcol for Xcol in df_nonnan_nonzerot0.columns if ipt.get_X_col(Xcol)]]
     y = df_nonnan_nonzerot0.loc[df_nonnan_nonzerot0["TRT_Rank|%i" % pred_dt]>=0.15,
                                 ["TRT_Rank_diff|%i" % pred_dt]]
+    """
     del(df_nonnan_nonzerot0)
-    
+
     ## Setup model:
     print("  Setup XGBmodel with max_depth = 6")
     xgb_model = xgb.XGBRegressor(max_depth=6,silent=False,n_jobs=6,nthreads=6)
@@ -118,12 +115,12 @@ def get_feature_importance(df_nonnan_nonzerot0,pred_dt,cfg_tds,model_path,mod_bo
     d_start = dt.datetime.now()
     xgb_model.fit(X, y, verbose=True)
     print("    Elapsed time for XGBoost model fitting: %s" % (dt.datetime.now()-d_start))
-    
+
     ## Save model to disk:
     print("  Save XGBmodel to disk")
     with open(os.path.join(model_path,"model_%i%s_t0diff_maxdepth6.pkl" % (pred_dt,mod_name)),"wb") as file:
         pickle.dump(xgb_model,file,protocol=-1)
-        
+
     ## Plot feature importance:
     print("  Plot feature importance")
     plot_feature_importance(xgb_model,X,pred_dt,cfg_tds,mod_name)
@@ -137,7 +134,7 @@ def get_mse_from_n_feat(df_nonnan_nonzerot0,pred_dt,cfg_tds,model_path,mod_bound
     else:
         print(" (for all samples)")
     sys.stdout.flush()
-    
+
     ## Check whether data on MSE already exists:
     if os.path.exists(os.path.join(model_path,"MSE_feature_count_gain_%i%s.pkl" % (pred_dt,mod_name))):
         use_existing = ""
@@ -146,21 +143,26 @@ def get_mse_from_n_feat(df_nonnan_nonzerot0,pred_dt,cfg_tds,model_path,mod_bound
         if use_existing=="n":
             print("  Use existing one, return from this function")
             return
-            
+
     ## Delete rows with TRT Rank close to zero at lead time:
     print("  Delete rows with TRT Rank close to zero at lead time")
+    X_train, X_test, y_train, y_test = ipt.get_model_input(df_nonnan_nonzerot0,
+            del_TRTeqZero_tpred=True, split_Xy_traintest=True,
+            pred_dt=pred_dt, TRTRankt0_bound=mod_bound)
+    """
     if mod_bound is not None:
         df_nonnan_nonzerot0 = df_nonnan_nonzerot0.loc[(df_nonnan_nonzerot0["TRT_Rank|0"]>=mod_bound[0]) & \
                                                       (df_nonnan_nonzerot0["TRT_Rank|0"]<mod_bound[1])]
     X = df_nonnan_nonzerot0.loc[df_nonnan_nonzerot0["TRT_Rank|%i" % pred_dt]>=0.15,
-                                [Xcol for Xcol in df_nonnan_nonzerot0.columns if get_X_col(Xcol)]]
+                                [Xcol for Xcol in df_nonnan_nonzerot0.columns if ipt.get_X_col(Xcol)]]
     y = df_nonnan_nonzerot0.loc[df_nonnan_nonzerot0["TRT_Rank|%i" % pred_dt]>=0.15,
                                 ["TRT_Rank_diff|%i" % pred_dt]]
     del(df_nonnan_nonzerot0)
-    
+
     ## Split training/testing data:
     print("  Split into training/testing data")
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    """
     del(X,y)
 
     ## Load XGBmodel:
@@ -178,7 +180,7 @@ def get_mse_from_n_feat(df_nonnan_nonzerot0,pred_dt,cfg_tds,model_path,mod_bound
                                  np.arange(12,50,2),
                                  np.arange(50,100,10),
                                  np.arange(100,520,20)])
-    
+
     ## Get models fitted with n top features:
     print("  Get models fitted with n top features")
     ls_models = [fit_model_n_feat(X_train, y_train, top_features_gain, n_feat, n_feat_arr) for n_feat in n_feat_arr]
@@ -187,16 +189,16 @@ def get_mse_from_n_feat(df_nonnan_nonzerot0,pred_dt,cfg_tds,model_path,mod_bound
         pickle.dump(ls_models, file, protocol=-1)
     #with open(os.path.join(model_path,"models_%i_t0diff_maxdepth6_nfeat.pkl" % pred_dt),"rb") as file:
     #    ls_models = pickle.load(file)
-    
+
     ## Get mean square error of models with n features:
     print("  Get mean square error of models with n features")
-    MSE_gain_ls = [mse_n_feat(X_test, y_test, top_features_gain, n_feat, model) for n_feat, model in zip(n_feat_arr,ls_models)]    
+    MSE_gain_ls = [mse_n_feat(X_test, y_test, top_features_gain, n_feat, model) for n_feat, model in zip(n_feat_arr,ls_models)]
     df_mse_feat_count = pd.DataFrame.from_dict({"Feature Count": n_feat_arr,
                                                 "MSE %imin%s" % (pred_dt,mod_name): MSE_gain_ls})
     df_mse_feat_count.set_index("Feature Count",inplace=True)
     print("    Save dataframe with MSE to disk")
     with open(os.path.join(model_path,"MSE_feature_count_gain_%i%s.pkl" % (pred_dt,mod_name)),"wb") as file: pickle.dump(df_mse_feat_count,file,protocol=-1)
-    
+
     ## Append MSE values to existing HDF5 file (if existing):
     print("  Append MSE values to HDF5 file")
     df_mse_feat_count.to_hdf(os.path.join(model_path,"MSE_feature_count_gain.h5"),
@@ -207,13 +209,13 @@ def get_mse_from_n_feat(df_nonnan_nonzerot0,pred_dt,cfg_tds,model_path,mod_bound
     #else:
     #    store = pd.HDFStore(os.path.join(model_path,"MSE_feature_count_gain.pkl"))
     #    store.append(df_mse_feat_count, ohlcv_candle, format='t',  data_columns=True)
-        
+
 ## Plot dependence of MSE on feature number:
 def plot_mse_from_n_feat(ls_pred_dt,cfg_tds,model_path,thresholds=None,ls_model_names=[""]):
     print("Plot MSE as function of number of features used")
     if ls_model_names != [""]:
         ls_model_names = ["_%s" % name for name in ls_model_names]
-    
+
     ## Read in MSE values:
     print("  Reading MSE values from disk")
     ls_df_mse_feat_count = []
@@ -221,7 +223,7 @@ def plot_mse_from_n_feat(ls_pred_dt,cfg_tds,model_path,thresholds=None,ls_model_
         ls_df_mse_feat_count += [pd.read_hdf(os.path.join(model_path,"MSE_feature_count_gain.h5"),
                                              key="MSE_%imin%s" % (pred_dt,model_name), mode="r") for pred_dt in ls_pred_dt]
     df_mse_feat_count = pd.concat(ls_df_mse_feat_count, axis=1)
-    
+
     ## Plot the figure:
     print("  Plotting the figure")
     col10 = '#E69F00'
@@ -312,13 +314,14 @@ def plot_mse_from_n_feat(ls_pred_dt,cfg_tds,model_path,thresholds=None,ls_model_
     else:
         df_mse_feat_count.plot.line()
         plt.show()
-        
+
 ## Fit model with threshold number of features (selected by the user):
-def plot_pred_vs_obs(df_nonnan_nonzerot0,pred_dt,n_feat_ls,cfg_tds,model_path,ls_mod_bound=[None],ls_model_names=[""]):
+def plot_pred_vs_obs(df_nonnan_nonzerot0,pred_dt,n_feat_ls,cfg_tds,model_path,
+                     ls_mod_bound=[None],ls_model_names=[""]):
     if len(ls_mod_bound)>1:
         y_test_ls = []
         pred_gain_ls = []
-    
+
     for mod_bound, n_feat, mod_name in zip(ls_mod_bound,n_feat_ls,ls_model_names):
         print("\nPlot predicted vs. observed testing samples for time delta %imin" % (pred_dt),end="")
         if mod_bound is not None:
@@ -327,23 +330,28 @@ def plot_pred_vs_obs(df_nonnan_nonzerot0,pred_dt,n_feat_ls,cfg_tds,model_path,ls
         else:
             print(" (%i features for all samples)" & n_feat)
         sys.stdout.flush()
-        
+
         ## Delete rows with TRT Rank close to zero at lead time:
         print("  Delete rows with TRT Rank close to zero at lead time")
+        X_train, X_test, y_train, y_test = ipt.get_model_input(df_nonnan_nonzerot0,
+                del_TRTeqZero_tpred=True, split_Xy_traintest=True,
+                pred_dt=pred_dt, TRTRankt0_bound=mod_bound)
+        """
         if mod_bound is not None:
             df_nonnan_nonzerot0_mod = df_nonnan_nonzerot0.loc[(df_nonnan_nonzerot0["TRT_Rank|0"]>=mod_bound[0]) & \
                                                               (df_nonnan_nonzerot0["TRT_Rank|0"]<mod_bound[1])]
         X = df_nonnan_nonzerot0_mod.loc[df_nonnan_nonzerot0_mod["TRT_Rank|%i" % pred_dt]>=0.15,
-                                    [Xcol for Xcol in df_nonnan_nonzerot0_mod.columns if get_X_col(Xcol)]]
+                                    [Xcol for Xcol in df_nonnan_nonzerot0_mod.columns if ipt.get_X_col(Xcol)]]
         y = df_nonnan_nonzerot0_mod.loc[df_nonnan_nonzerot0_mod["TRT_Rank|%i" % pred_dt]>=0.15,
                                     ["TRT_Rank_diff|%i" % pred_dt]]
         del(df_nonnan_nonzerot0_mod)
-        
+
         ## Split training/testing data:
         print("  Split into training/testing data")
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        """
         del(X,y)
-        
+
         precalc_n_feat = np.concatenate([np.arange(10)+1, np.arange(12,50,2),
                                          np.arange(50,100,10), np.arange(100,520,20)])
         if n_feat not in precalc_n_feat:
@@ -356,33 +364,33 @@ def plot_pred_vs_obs(df_nonnan_nonzerot0,pred_dt,n_feat_ls,cfg_tds,model_path,ls
             top_features_gain = pd.DataFrame.from_dict(xgb_model.get_booster().get_score(importance_type='gain'),
                                                        orient="index",columns=["F_score"]).sort_values(by=['F_score'],
                                                        ascending=False)
-                                                       
+
             ## Fit model:
             model    = fit_model_n_feat(X_train, y_train, top_features_gain, n_feat, np.array(n_feat))
         else:
             with open(os.path.join(model_path,"models_%i%s_t0diff_maxdepth6_nfeat.pkl" % (pred_dt,mod_name)),"rb") as file:
                 model = pickle.load(file)[np.where(precalc_n_feat==n_feat)[0][0]]
-            
+
         ## Get features:
         features  = model.get_booster().feature_names
-        
+
         ## Make prediction and get skill scores:
         pred_gain = model.predict(X_test[features])
         mse_gain  = met.mean_squared_error(y_test[["TRT_Rank_diff|%i" % pred_dt]], pred_gain)
         r2_gain   = met.r2_score(y_test[["TRT_Rank_diff|%i" % pred_dt]], pred_gain)
-        
+
         ## Save the model to disk:
         with open(os.path.join(model_path,"model_%i%s_t0diff_maxdepth6_%ifeat_gain.pkl" % (pred_dt,mod_name,n_feat)),"wb") as file:
             pickle.dump(model,file,protocol=-1)
-        
+
         ## Make the plot:
         plot_pred_vs_obs_core(y_test,pred_gain,pred_dt,mse_gain,r2_gain,mod_name,cfg_tds)
-     
+
         ## Append to list of results for combined plot:
         if len(ls_mod_bound)>1:
             y_test_ls.append(y_test)
             pred_gain_ls.append(pred_gain)
-    
+
     ## Make combined plot:
     if len(ls_mod_bound)>1:
         y_test_combi = pd.concat(y_test_ls,axis=0)
@@ -391,19 +399,19 @@ def plot_pred_vs_obs(df_nonnan_nonzerot0,pred_dt,n_feat_ls,cfg_tds,model_path,ls
         r2_gain   = met.r2_score(y_test_combi[["TRT_Rank_diff|%i" % pred_dt]],pred_gain_combi)
         plot_pred_vs_obs_core(y_test_combi, pred_gain_combi,
                               pred_dt,mse_gain,r2_gain,"_%s" % "|".join(ls_model_names),cfg_tds)
-    
-        
+
+
 def plot_pred_vs_obs_core(y_test,pred_gain,pred_dt,mse_gain,r2_gain,mod_name,cfg_tds):
     print("  Making the plot")
     fig, axes = plt.subplots(nrows=1, ncols=1, figsize=[10,8])
     if len(y_test)>1000:
         counts,ybins,xbins,image = axes.hist2d(y_test[["TRT_Rank_diff|%i" % pred_dt]].values[:,0],pred_gain,
-                                               bins=200,range=[[-2.,2.],[-2.,2.]],cmap="magma",norm=mcolors.LogNorm())
+                                               bins=200,range=[[-2.5,2.5],[-2.5,2.5]],cmap="magma",norm=mcolors.LogNorm())
         cbar = fig.colorbar(image, ax=axes, extend='max')
     else:
         axes.scatter(y_test[["TRT_Rank_diff|%i" % pred_dt]].values[:,0],pred_gain,
                      marker="+", color="black", s=8)
-    axes.set_xlim([-2,2]); axes.set_ylim([-2,2])
+    axes.set_xlim([-2.5,2.5]); axes.set_ylim([-2.5,2.5])
     #cbar.set_label('Number of cells per bin of size [0.02, 0.02]', rotation=90)
     axes.grid()
     #axes.fill_between([-0.2,0.2],y1=[-1.5,-1.5], y2=[1.5,1.5], facecolor="none", hatch="X", edgecolor="darkred", linewidth=0.5)
@@ -424,12 +432,6 @@ def plot_pred_vs_obs_core(y_test,pred_gain,pred_dt,mse_gain,r2_gain,mod_name,cfg
     str_n_cells  = "Mean Squared Error (MSE): %.2f\n" % (mse_gain)
     str_n_cells += r"Coeff of determination ($R^2$): %.2f" % (r2_gain)
     props = dict(boxstyle='round', facecolor='white')
-    axes.text(-1.5,1.5, str_n_cells, bbox=props)
+    axes.text(-2,2, str_n_cells, bbox=props)
     plt.savefig(os.path.join(cfg_tds["fig_output_path"],"Pred_scatter_%i%s.pdf" % (pred_dt,mod_name.replace("|","-"))), orientation="portrait")
     print("    Saved the plot")
-
-
-    
-    
-    
-    
