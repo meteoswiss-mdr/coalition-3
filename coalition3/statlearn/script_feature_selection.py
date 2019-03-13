8183f064c11e943a528e1fc957b69ce03a21e4f0
Joel Miro Zeder <Joel.Miro@meteoswiss.ch>
2019-03-13 22:59:53 +0000

    Temporary merge with 'script_fit_ann.py'

--- coalition3/statlearn/script_feature_selection.py
+++ coalition3/statlearn/script_feature_selection.py
@@ -76,7 +76,39 @@ for pred_dt in ls_pred_dt:
 ## Plot MSE as function of number of features:
 feat.plot_mse_from_n_feat(ls_pred_dt,cfg_tds,model_path,thresholds=None,
                           ls_model_names=ls_model_names)
+"""
 
+
+model_path = "/data/COALITION2/PicturesSatellite/results_JMZ/0_training_NOSTRADAMUS_ANN/statistical_learning/ANN_models/models/diam_23km/without_radar_t0" #pth.file_path_reader("model saving location")
+for pred_dt in ls_pred_dt:
+    ## Get normalised training and testing data:
+    X_train, X_test, y_train, y_test, scaler = ipt.get_model_input(df_nonnan,
+        del_TRTeqZero_tpred=True, split_Xy_traintest=True, X_normalise=True,
+        pred_dt=pred_dt)
+        
+    ## Fit ANN models with 10 - 1000 selected features with grid-search over hyperparameters:
+    print("Fit ANN models to 10 - 1000 features with grid-search over hyper-parameters")
+    xgb_model_path_all = "/data/COALITION2/PicturesSatellite/results_JMZ/0_training_NOSTRADAMUS_ANN/statistical_learning/feature_selection/models/diam_23km/without_radar_t0/all_samples/model_%i_t0diff_maxdepth6.pkl" % pred_dt #pth.file_path_reader("all-feature XGB model location (for feature selection)")
+
+    with open(xgb_model_path_all,"rb") as file:
+        xgb_model = pickle.load(file)
+    top_features_gain = pd.DataFrame.from_dict(xgb_model.get_booster().get_score(importance_type='gain'),
+                                               orient="index",columns=["F_score"]).sort_values(by=['F_score'],
+                                               ascending=False)
+    n_feat_arr = feat.get_n_feat_arr("mlp")
+    print("  *** Watch out, this takes veeeeeeeeeeeeery long!! ***")
+    ls_models = []
+    time_start = dt.datetime.now()
+    for n_feat in n_feat_arr:
+        print("\n     Current time: %s / elapsed time %s" % (dt.datetime.now(), dt.datetime.now()-time_start))
+        fitted_model = feat.fit_model_n_feat(X_train, y_train, top_features_gain, n_feat, n_feat_arr, model="mlp", verbose_bool=False)
+        ls_models.append(fitted_model)
+        if n_feat%20==0:
+            with open(os.path.join(model_path,"model_%i%s_t0diff_mlp_nfeat_%i.pkl" % (pred_dt,mod_name,n_feat)),"wb") as file:
+                pickle.dump(ls_models,file,protocol=-1)
+       
+
+"""
 ## Fit model with optimal number of features:
 poss_n_feat = np.arange(1,501)
 ls_n_feat_dt = []
